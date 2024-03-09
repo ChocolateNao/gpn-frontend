1566e572ba7134fd675b0b9fea0af1e9153ee68f
Andrey Loginov <andrey12q112@gmail.com>
2024-03-09 21:42:04 +0300

    feat: implement data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import CurrencySelect from './components/CurrencySelect';
 import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
 import ExchangeRatesChart from './components/ExchangeRatesChart';
-import { mockData } from './data/data';
 import { CurrencyTypeProvider } from './context/currencyTypeContext';
 import ChartTitle from './components/ChartTitle';
-import './App.css';
 import AvgRate from './components/AvgRate';
+import type { IExchangeRateData } from './models/exchangeRate.interface';
+
+import { useFetchData } from './hooks/useFetchData';
+import './App.css';
+import Loader from './components/Loader';
+import FetchError from './components/FetchError';
 
 function App() {
+  const { data, loading, errorCode } = useFetchData(
+    'https://65ec4b420ddee626c9afecaf.mockapi.io/api/rates'
+  );
+
+  if (errorCode)
+    return (
+      <div className="app__container">
+        <FetchError statusCode={errorCode} />
+      </div>
+    );
+
   return (
     <Theme preset={presetGpnDefault}>
       <CurrencyTypeProvider>
         <div className="app__container">
-          <header className="app__header">
-            <ChartTitle data={mockData} />
-            <CurrencySelect />
-          </header>
-          <section className="app__chart">
-            <ExchangeRatesChart data={mockData} />
-            <AvgRate data={mockData} />
-          </section>
+          {loading ? (
+            <Loader />
+          ) : (
+            <>
+              <header className="app__header">
+                <ChartTitle data={data as IExchangeRateData[]} />
+                <CurrencySelect />
+              </header>
+              <section className="app__chart">
+                <ExchangeRatesChart data={data as IExchangeRateData[]} />
+                <AvgRate data={data as IExchangeRateData[]} />
+              </section>
+            </>
+          )}
         </div>
       </CurrencyTypeProvider>
     </Theme>
