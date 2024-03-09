d92f944018cec53f00f876f52072c31ca5295cab
Andrey Loginov <andrey12q112@gmail.com>
2024-03-09 19:45:12 +0300

    feat: add logic to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import CurrencySelect from './components/CurrencySelect';
 import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
+import ExchangeRatesChart from './components/ExchangeRatesChart';
+import { mockData } from './data/data';
+import { CurrencyTypeProvider } from './context/currencyTypeContext';
+import ChartTitle from './components/ChartTitle';
+import './App.css';
+import AvgRate from './components/AvgRate';
 
 function App() {
   return (
     <Theme preset={presetGpnDefault}>
-      <CurrencySelect />
+      <CurrencyTypeProvider>
+        <div className="app__container">
+          <header className="app__header">
+            <ChartTitle data={mockData} />
+            <CurrencySelect />
+          </header>
+          <section className="app__chart">
+            <ExchangeRatesChart data={mockData} />
+            <AvgRate data={mockData} />
+          </section>
+        </div>
+      </CurrencyTypeProvider>
     </Theme>
   );
 }
